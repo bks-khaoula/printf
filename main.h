8ba0fe9c9b361f11e53ee0378affca345f37d0bd
Khaoula <bakasskhaoula9@gmail.com>
2023-04-25 16:04:21 -0700

    header

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+
+#endif
