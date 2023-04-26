c108a297196cb12bc85786c3a77da58c11748711
Your Name <you@example.com>
2023-04-26 15:12:20 -0700

    writer func

--- writer_printf.c
+++ writer_printf.c
@@ -38,3 +38,28 @@ void	write_str(char *str, int *count)
 		}
 	}
 }
+
+/**
+ * write_nbr - Funct that print the number
+ *
+ * @n: the number.
+ * @count: number of number.
+ */
+
+void	write_nbr(int n, int *count)
+{
+	if (n >= 0 && n < 10)
+		write_char((n + '0'), count);
+	else if (n == -2147483648)
+		*count += write(1, "-2147483648", 11);
+	else if (n >= 10)
+	{
+		write_nbr((n / 10), count);
+		write_nbr((n % 10), count);
+	}
+	else if (n < 0)
+	{
+		write_char(('-'), count);
+		write_nbr((n * -1), count);
+	}
+}
