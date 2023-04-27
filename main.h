42e01971907671c35255ebac625021a106e0db08
Your Name <you@example.com>
2023-04-27 16:25:49 -0700

    function print

--- main.h
+++ main.h
@@ -9,8 +9,7 @@
 void write_char(char c, int *count);
 void write_str(char *str, int *count);
 void write_nbr(int n, int *count);
-int char_check(char const c);
-void _print_cases(const char form, va_list args, int *count);
+int char_check(char const c);void _print_cases(const char form, va_list args, int *count);
 int _printf(const char *format, ...);
 
 #endif
